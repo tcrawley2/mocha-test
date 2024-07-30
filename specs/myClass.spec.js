a6e2cc59d156cfa5fbc4c77948d4d729d7159fae
Tim Crawley Jr <timcrawley@Tims-MacBook-Pro.local>
2024-07-30 15:06:45 -0700

    added more functions to MyClass and test suite

--- specs/myClass.spec.js
+++ specs/myClass.spec.js
@@ -6,4 +6,13 @@ describe ("Test suite", function () {
   it("Test the add method", function () {
     expect(myObj.add (1, 2)).to.be.equal(3);
   });
+  it("Test the subtract method", function () {
+    expect(myObj.subtract (10, 3)).to.be.equal(7);
+  });
+  it("Test the multiply method", function () {
+    expect(myObj.multiply (5, 6)).to.be.equal(30);
+  });
+  it("Test the divide method", function () {
+    expect(myObj.divide (1, 2)).to.be.equal(.5);
+  });
 });
--- src/myClass.js
+++ src/myClass.js
@@ -7,6 +7,27 @@ class MyClass {
     const result = arg1 + arg2;
     return result;
   }
+
+  subtract (arg1, arg2) {
+    const result = arg1 - arg2;
+    return result;
+  }
+
+  multiply (arg1, arg2) {
+    const result = arg1 * arg2;
+    return result;
+  }
+
+  divide (arg1, arg2) {
+    if (arg2 == 0) {
+      console.log("ERROR: Cannot divide by zero.");
+      return undefined;
+    }
+    else {
+      const result = arg1 / arg2;
+      return result;
+    }
+  }
 }
 
 export default MyClass;
